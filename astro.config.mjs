900951b02cbe839083752583423e9cb165eb8b38
Rafael Mestre <rafaelmestre@outlook.com>
2023-09-20 13:45:15 -0400

    astro add mdx && npm i mdx@1.0.2 && astro add @analogjs/astro-angular

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import analogjsangular from "@analogjs/astro-angular";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), analogjsangular()]
+});
